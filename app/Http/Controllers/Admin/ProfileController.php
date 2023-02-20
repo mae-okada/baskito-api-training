7067bedfcda2e5a22c205bd35d15824fbc315e1b
Aryan Jaya <aryanjaya88@gmail.com>
2023-02-20 11:45:09 +0800

    move user & profile controller

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\UpdateProfileRequest;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\Auth\LoginController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 
