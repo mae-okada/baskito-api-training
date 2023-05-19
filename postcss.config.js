7ebf165c4a3ff3f5e67bac114e0961176ddb2a33
Aryan Jaya <aryanjaya88@gmail.com>
2023-05-19 11:56:05 +0800

    add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+    purgecss({
+      content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/views/**/*.vue",
+        "./resources/scripts/**/*.js",
+        "./resources/scripts/**/*.ts",
+      ],
+    }),
+  ],
+};
